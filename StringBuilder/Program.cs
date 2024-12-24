1ed78c714a536de92d94e81c481b846c5452f247
itrakesh2004 <rrveerit2004@gmail.com>
2024-12-24 11:34:26 +0530

    add StringBuilder

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // string str = "Hello World!"; // Create a string variable
+        // string  str2=str; copy kari new memeory create hoga
+        // Console.WriteLine(str2);
+
+        
+        //multiple time update value string use for string builder 
+
+
+        StringBuilder sb = new ("Hello"); // Create a StringBuilder object 
+        sb.Append(" How ");
+        Console.WriteLine(sb);
+        Console.WriteLine($"Length:{sb.Length}");
+        Console.WriteLine($"Capacity:{sb.Capacity}");
+
+}
+}
