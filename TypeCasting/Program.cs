fdb96cdf59e97bc28474b47a58e292fbb3be1c60
itrakesh2004 <rrveerit2004@gmail.com>
2024-12-24 10:18:09 +0530

    add TypeCating

---
+++ TypeCasting/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string str="1234";
+        int num=Convert.ToInt32(str);
+        Console.WriteLine(num);
+
+
+
+
+        string str1 = "1234.5";
+        double num1 = Convert.ToDouble(str1); // Convert to double
+        int num2 = (int)num1;                 // Explicitly cast double to int
+        Console.WriteLine(num2);
+     
+
+        string str2="1234.5";
+        decimal num3=Convert.ToDecimal(str2);
+        Console.WriteLine(num3);
+
+    }
+}
