5f573e9b62cec03e31ceb325dd0392b4cc2e15d1
itrakesh2004 <rrveerit2004@gmail.com>
2024-12-24 11:20:32 +0530

    add formet

---
+++ StringFormet/Program.cs
@@ -0,0 +1,16 @@
+﻿//string Formet
+
+int age=21;
+string name="Rakesh";
+
+string str=string.Format("My name is {0} and my age is {1}",name,age);
+Console.WriteLine(str);
+
+//string Interpolation
+
+int age1=21;
+string name1="Rakesh";
+
+string str1=$"My name is {name1} and my age is {age1}";
+Console.WriteLine(str1);
+
