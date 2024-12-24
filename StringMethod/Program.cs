c44ae2dced2707e2a8e3b4172851a6a7d2b755a2
itrakesh2004 <rrveerit2004@gmail.com>
2024-12-24 11:14:07 +0530

    add methods

---
+++ StringMethod/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        string str = "Hello World";
+        Console.WriteLine(str.Length); //Length
+
+        Console.WriteLine(str.ToUpper()); //Upper
+        Console.WriteLine(str.ToLower()); //Lower
+
+        Console.WriteLine(str.Contains("World")); //Contains
+
+        Console.WriteLine(str.StartsWith("Hello")); //StartsWith
+
+        Console.WriteLine(str.EndsWith("World")); //EndsWith
+
+        Console.WriteLine(str.Substring(6, 5)); //Substring
+
+        Console.WriteLine(str.Replace("World", "Worlds")); //Replace
+
+        Console.WriteLine(str.Trim()); //Trim
+
+        Console.WriteLine(str.IndexOf("World")); //IndexOf
+
+        Console.WriteLine(str.LastIndexOf("World")); //LastIndexOf
+
+        Console.WriteLine(str.Insert(6, "Worlds")); //Insert
+
+        string input = "this is a sentence."; // Split
+        string[] words = input.Split(' ');
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
+
+
+    }
+}
