faf70beba8f373d7223430eb5a87fc2b17a2bd39
itrakesh2004 <rrveerit2004@gmail.com>
2024-12-25 11:36:33 +0530

    classes

---
+++ Classes/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Person p=new();
+        p.Name="ram";
+        p.Age=20;
+        Console.WriteLine(p.Name);
+        Console.WriteLine(p.Age);
+
+    }
+}
+
+class Person
+{
+    public string? Name  ;
+    public int? Age;
+
+}
--- Keyword/Program.cs
+++ Keyword/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine(sum(1,2,3,4,5));
         int number=10;
         Foo(number);
         Bar(out number);
@@ -19,7 +20,19 @@ class Program
     static void Bar(out int n) //out keyword
     {
         n=20;
-        Console.WriteLine(n);}
+        Console.WriteLine(n);
+        
+    }
 
+    //params
 
+    static int sum (params int[] numbers)
+    {
+        int total=0;
+        foreach(int number in numbers)
+        {
+            total+=number;
+        }
+        return total;
+    }
 }
