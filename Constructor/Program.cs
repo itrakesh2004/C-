79c08d607b60ab091bb0d37920f3ecca5c654969
itrakesh2004 <rrveerit2004@gmail.com>
2024-12-25 11:54:55 +0530

    add Constructor

---
+++ Constructor/Program.cs
@@ -0,0 +1,28 @@
+﻿class Person
+{
+     private string name;
+     private int age;
+
+     //Constructor
+
+     public Person(string name, int age)
+     {
+        this.name=name;
+        this.age=age;
+     }
+
+     public void Display()
+     {
+        Console.WriteLine($"name:{name} || age: {age}");
+     }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+       Person p=new("ram",20);
+       p.Display();
+
+    }
+}
