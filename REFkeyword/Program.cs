942e597aed1b617484ea9f50f36a52cebf2a23b9
itrakesh2004 <rrveerit2004@gmail.com>
2024-12-25 10:05:17 +0530

    add Ref

---
+++ REFkeyword/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int number=10;
+        ref int refNumber=ref number;  //referance create karvathi memory nave use kartu nathi
+        refNumber=20;
+        Console.WriteLine(number);
+        refNumber=30;
+        Console.WriteLine(number);
+    }
+}
