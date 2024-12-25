c8192f67a7ad0e4aca82c41705354258b8a613cb
itrakesh2004 <rrveerit2004@gmail.com>
2024-12-25 10:52:30 +0530

    Method Overloading

---
+++ MethodOverLoading/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int result = add(10, 20);
+        Console.WriteLine(result);
+        result = add(10, 20, 30);
+        Console.WriteLine(result);
+    }
+
+    static int add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    static int add(int num1, int num2, int num3)
+    {
+        return num1 + num2 + num3;
+    }
+}
