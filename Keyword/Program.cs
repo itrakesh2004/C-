1566547fae172760d7f80acedff5024cb1f48d1a
itrakesh2004 <rrveerit2004@gmail.com>
2024-12-25 10:28:08 +0530

    add KeyWord in and Out

---
+++ Keyword/Program.cs
@@ -0,0 +1,25 @@
+﻿//IN keyword
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number=10;
+        Foo(number);
+        Bar(out number);
+    }
+    static void Foo(in int n) //in keyword
+    {
+        //n=12; error
+        int other=n;
+        Console.WriteLine(other);
+
+    }
+
+    static void Bar(out int n) //out keyword
+    {
+        n=20;
+        Console.WriteLine(n);}
+
+
+}
