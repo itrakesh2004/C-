7e0cc868d40732e16287b7f9d0443eb3318f7894
itrakesh2004 <rrveerit2004@gmail.com>
2024-12-25 12:03:02 +0530

    Inheritanc

---
+++ Inheritance/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("eating...");
+    }
+}
+
+public class Dog : Animal
+{
+    public void Bark()
+    {
+        Console.WriteLine("barking...");
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Dog d = new Dog();
+        d.Bark();
+        d.Eat();
+    }
+}
