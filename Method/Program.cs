7deec9b208c0c9f284d3cf5970112c5daab479b0
itrakesh2004 <rrveerit2004@gmail.com>
2024-12-25 09:54:50 +0530

    add Method

---
+++ Method/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Hello("RRR");
+        Console.WriteLine(Add(2,3));
+    }
+
+    // Parameter and Argument
+    static void Hello(string name)
+    {
+        Console.WriteLine($"Hello World{name}");
+    }
+
+    // return value
+
+    static int Add(int num1,int num2)
+    {
+        int sum=num1+num2;
+        return sum;
+    }
+    
+}
