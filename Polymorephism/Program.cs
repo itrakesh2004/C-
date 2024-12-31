f137b6774b19a0a33134705d3e143732590e20cf
itrakesh2004 <rrveerit2004@gmail.com>
2024-12-31 13:42:53 +0530

    Polymorephism

---
+++ Polymorephism/Program.cs
@@ -0,0 +1,31 @@
+﻿interface IPerson
+{
+    void Display();
+}
+
+class Person : IPerson
+{
+    public void Display()
+    {
+        Console.WriteLine("Person");
+    }
+}
+
+class Student : IPerson
+{
+    public void Display()
+    {
+        Console.WriteLine("Student");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IPerson p = new Person(); //upcasting polymorephism
+        p.Display();
+        p = new Student(); //reference type
+        p.Display();
+    }
+}
