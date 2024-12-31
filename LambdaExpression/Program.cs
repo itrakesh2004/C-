569949214577a9d9d08ddb460b31dc5f57a8194f
itrakesh2004 <rrveerit2004@gmail.com>
2024-12-31 14:26:24 +0530

    Lambda Expression

---
+++ LambdaExpression/Program.cs
@@ -0,0 +1,18 @@
+﻿// two type of lambda expression
+
+var sum = (int a, int b) => a + b; //lambda expression
+Console.WriteLine(sum(10, 20));
+
+var sum2 = (int a, int b) => { return a + b; }; //lambda expression
+Console.WriteLine(sum2(10, 20));
+
+
+////
+
+List<int> num=new(){1,2,3,4,5}; //list
+List<int> odd = num.Where(x => x % 2 != 0).ToList(); //lambda
+
+foreach (int i in odd)
+{
+    Console.WriteLine(i);
+}
