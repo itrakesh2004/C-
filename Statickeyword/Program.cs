94e85926020535eb928dd7ae2db8c06b56ee885f
itrakesh2004 <rrveerit2004@gmail.com>
2024-12-31 13:15:49 +0530

    Static Keyword

---
+++ Statickeyword/Program.cs
@@ -0,0 +1,31 @@
+﻿class Person
+{
+    public static string name="ram";
+
+    public static void Display()
+    {
+        Console.WriteLine($"name:{name}");
+    }
+}
+
+class Student 
+{
+    public static void std()
+    {
+        Console.WriteLine("student");
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //not use ststatic keyword
+        // Person p=new Person();
+        // p.Display();
+
+        //use static keyword
+        Person.Display();               
+        Student.std();
+    }
+}
