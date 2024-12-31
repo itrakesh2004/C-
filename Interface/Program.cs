d16489adadc92cf549987fb90f573af9aff52014
itrakesh2004 <rrveerit2004@gmail.com>
2024-12-31 13:25:05 +0530

    InterFace

---
+++ Interface/Program.cs
@@ -0,0 +1,28 @@
+﻿interface Ivehicle
+{
+    void drive();
+    void stop();
+}                   
+
+class Car : Ivehicle
+{
+    public void drive()
+    {
+        Console.WriteLine("car is driving");
+    }
+    public void stop()
+    {
+        Console.WriteLine("car is stopped");
+    }
+    
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car c = new Car();
+        c.drive();
+        c.stop();
+    }
+}
