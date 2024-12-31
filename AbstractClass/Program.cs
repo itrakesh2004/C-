83460f9cee1fe5190c63ce76d2f1d9e55195b3ce
itrakesh2004 <rrveerit2004@gmail.com>
2024-12-31 13:05:14 +0530

    Abstractclass

---
+++ AbstractClass/Program.cs
@@ -0,0 +1,32 @@
+﻿abstract class Animal
+{
+    public abstract void Eat();  // abstract method aa method badha class ma vaprvij pade
+    public void sleep()
+    {
+        Console.WriteLine("aleeping...");
+    }
+}
+class Pig : Animal
+{
+    public override void Eat()
+    {
+        Console.WriteLine("eating...");
+    }
+
+    public void sleep2()
+    {
+        Console.WriteLine("sleeping...");
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Pig p = new Pig();
+        p.Eat();
+        p.sleep();
+        p.sleep2();
+
+    }
+}
