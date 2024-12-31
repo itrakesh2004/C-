18e3f9ae9a6fe103295659ddffa55b6fe544866c
itrakesh2004 <rrveerit2004@gmail.com>
2024-12-31 14:06:15 +0530

    Delegate

---
+++ Delegate/Program.cs
@@ -0,0 +1,24 @@
+﻿class MainProgram
+{
+    static void Alert(string message) //delegate
+    {
+        Console.WriteLine(message);
+    }
+
+    static void Alert2(string message) //delegate
+    {
+        Console.WriteLine(message);
+    }
+
+    delegate void AlertDelegate(string message); //delegate type
+
+    static void Main(string[] args)
+    {
+        AlertDelegate ad = Alert; //not create object but only Refernce
+
+        ad.Invoke("Hello");
+        ad += Alert2;
+        ad.Invoke("Hello2");
+
+    }
+}
