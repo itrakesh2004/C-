3b694ef646f9fe9be6e3a731c6a6ec805d4f4834
itrakesh2004 <rrveerit2004@gmail.com>
2024-12-31 14:16:10 +0530

    Exception

---
+++ Exception/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            bool a = false;
+            bool other = true;
+            if(a)
+            {
+                throw new Exception("exception");
+            }
+            else if (other)
+            {
+                throw new ArgumentException("other exception");
+            }
+            else
+            {
+                Console.WriteLine("no exception");
+            }
+           
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
